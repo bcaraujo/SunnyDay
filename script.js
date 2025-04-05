7adafec50f617c3d426e4a739a8e127d67e6929c
sania <sania.aslam2514@gmail.com>
2025-04-05 14:15:51 +0200

    hello

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const counterElement = document.getElementById('counter');
 const incrementButton = document.getElementById('increment'); 
 const decrementButton = document.getElementById('decrement'); 
  
-let count = 0; 
+let count = 20; 
  
 incrementButton.addEventListener('click', () => { 
     count++; 
